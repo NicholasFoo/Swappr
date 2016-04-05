fa3b031282058795135afc3c48bcfa97b714db9c
mwei0210 <mwei0210@gmail.com>
2016-04-05 17:01:19 +0800

    done login, design you all kao dim hehe
    -requires webservice to login, which is with me
    -design you all kaodim
    -refer to my code to request data from webservice

--- src/com/example/swappr/LoginActivity.java
+++ src/com/example/swappr/LoginActivity.java
@@ -1,21 +1,39 @@
 package com.example.swappr;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.LoaderManager.LoaderCallbacks;
-import android.content.ContentResolver;
 import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -25,20 +43,17 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import java.util.ArrayList;
-import java.util.List;
+import android.widget.Toast;
+
+interface AsyncResponse {
+	void processFinish(String output);
+}
 
 /**
  * A login screen that offers login via email/password.
  * 
  */
-public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
-
-	/**
-	 * A dummy authentication store containing known user names and passwords.
-	 * TODO: remove after connecting to a real authentication system.
-	 */
-	private static final String[] DUMMY_CREDENTIALS = new String[] { "foo@example.com:hello", "bar@example.com:world" };
+public class LoginActivity extends Activity implements LoaderCallbacks<Cursor>, AsyncResponse {
 	/**
 	 * Keep track of the login task to ensure we can cancel it if requested.
 	 */
@@ -54,7 +69,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-
+		// this to set delegate/listener back to this class
 		// Set up the login form.
 		mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
 		populateAutoComplete();
@@ -135,7 +150,9 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 			// perform the user login attempt.
 			showProgress(true);
 			mAuthTask = new UserLoginTask(email, password);
+			mAuthTask.delegate = this;
 			mAuthTask.execute((Void) null);
+
 		}
 	}
 
@@ -240,10 +257,11 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 	 * Represents an asynchronous login/registration task used to authenticate
 	 * the user.
 	 */
-	public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+	public class UserLoginTask extends AsyncTask<Void, Void, String> {
 
 		private final String mEmail;
 		private final String mPassword;
+		public AsyncResponse delegate = null;
 
 		UserLoginTask(String email, String password) {
 			mEmail = email;
@@ -251,38 +269,75 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 		}
 
 		@Override
-		protected Boolean doInBackground(Void... params) {
+		protected String doInBackground(Void... params) {
 			// TODO: attempt authentication against a network service.
 
+			/*
+			 * try { // Simulate network access. Thread.sleep(2000); } catch
+			 * (InterruptedException e) { return false; }
+			 * 
+			 * for (String credential : DUMMY_CREDENTIALS) { String[] pieces =
+			 * credential.split(":"); if (pieces[0].equals(mEmail)) { // Account
+			 * exists, return true if the password matches. return
+			 * pieces[1].equals(mPassword); } }
+			 */
+
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpPost httppost = new HttpPost("http://192.168.1.7/swappr/public/api/login");
+			HttpResponse response = null;
+			InputStream instream = null;
+			String responseText = null, token = null, result = null;
+
+			List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+			pairs.add(new BasicNameValuePair("email", mEmail));
+			pairs.add(new BasicNameValuePair("password", mPassword));
+
+			// Encoding POST data
 			try {
-				// Simulate network access.
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				return false;
+				httppost.setEntity(new UrlEncodedFormEntity(pairs));
+			} catch (UnsupportedEncodingException e) {
+				// log exception
+				e.printStackTrace();
 			}
 
-			for (String credential : DUMMY_CREDENTIALS) {
-				String[] pieces = credential.split(":");
-				if (pieces[0].equals(mEmail)) {
-					// Account exists, return true if the password matches.
-					return pieces[1].equals(mPassword);
-				}
+			// making POST request.
+			try {
+				response = httpclient.execute(httppost);
+				HttpEntity entity = response.getEntity();
+				responseText = EntityUtils.toString(entity);
+				JSONObject json = new JSONObject(responseText); // Convert
+																// String to
+																// JSON Object
+				token = json.getString("token");
+				// write response to log
+				// Log.d("Http Post Response:", responseText);
+			} catch (ClientProtocolException e) {
+				// Log exception
+				e.printStackTrace();
+			} catch (IOException e) {
+				// Log exception
+				e.printStackTrace();
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-
 			// TODO: register the new account here.
-			return true;
+			return token;
 		}
 
 		@Override
-		protected void onPostExecute(final Boolean success) {
+		protected void onPostExecute(final String token) {
 			mAuthTask = null;
 			showProgress(false);
 
-			if (success) {
-				finish();
+			if (!token.equals("invalid")) {
+				// finish();
+				delegate.processFinish(token);
 			} else {
-				mPasswordView.setError(getString(R.string.error_incorrect_password));
-				mPasswordView.requestFocus();
+				// mEmailView.setError(getString(R.string.error_invalid_email));
+				// mPasswordView.setError(getString(R.string.error_incorrect_password));
+				// mPasswordView.requestFocus();
+				Toast.makeText(getApplicationContext(), "Invalid credentials provided.", Toast.LENGTH_LONG).show();
 			}
 		}
 
@@ -291,5 +346,14 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 			mAuthTask = null;
 			showProgress(false);
 		}
+	}
+
+	// this override the implemented method from asyncTask
+	public void processFinish(String token) {
+		// Here you will receive the result fired from async class
+		// of onPostExecute(result) method.
+		Toast.makeText(getApplicationContext(), (String) token, Toast.LENGTH_LONG).show();
+
+		// pass token to next activity
 	}
 }
